c6b0e200479dd16bcc3d6a250548cdbc42041dd0
Flo-Nz <63399869+Flo-Nz@users.noreply.github.com>
2025-03-14 17:03:07 +0100

    rework of home page (#9)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import { Button, Card, CardBody, Image } from "@heroui/react";
-import { PlusIcon } from "../components/Icons";
+import {
+  AskOropIcon,
+  CartIcon,
+  DbIcon,
+  MyRatingsIcon,
+  PlusIcon,
+  SearchIcon,
+} from "../components/Icons";
 import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import { FormattedMessage, useIntl } from "react-intl";
-import parse from "html-react-parser";
+import { delay } from "lodash";
 
 const Home = () => {
-  const intl = useIntl();
+  const navigate = useNavigate();
 
   return (
     <div id="intro">
@@ -16,7 +23,7 @@ const Home = () => {
       >
         <CardBody>
           <div className="flex flex-col gap-0 text-center">
-            <h1 className="font-medium text-xl font-semibold">FIRSTPLAYER</h1>
+            <h1 className="font-medium text-2xl font-semibold">FIRSTPLAYER</h1>
             <p className="gap-0 text-small italic text-center">
               <i>
                 <FormattedMessage id="Home.Subtitle" />
@@ -24,28 +31,69 @@ const Home = () => {
             </p>
           </div>
 
-          <div className="flex flex-col gap-0 items-center lg:items-start">
-            <div className="flex flex-col gap-0">
-              <h1 className="font-medium text-xl mt-6 lg:mt-0">
-                <FormattedMessage id="Home.WhoAmI" />
-              </h1>
-              <p className="gap-0 text-small italic text-center lg:text-start">
-                <i>Yoël Sayada</i>
-              </p>
-            </div>
-            <div className="pt-4 pb-2 grid grid-flow-row gap-4 grid-cols-1 lg:grid-flow-col lg:grid-cols-5">
-              <div className="col-span-2 m-auto lg:col-start-1 lg:col-end-1 lg:col-span-1 lg:m-0">
-                <Image
-                  alt="Profile picture"
-                  className="object-cover"
-                  shadow="sm"
-                  src="./yoel.jpg"
-                  width={200}
-                />
-              </div>
-              <div className="lg:col-span-4 m-auto">
-                {parse(intl.formatMessage({ id: "Home.Intro" }))}
-              </div>
+          <div className="mt-4 text-center">
+            <h2>
+              Rechercher des épisodes de l'émission "On rejoue ou pas ?", faire
+              ta propre notation ou demander à Yoël de traiter un jeu spécifique
+              ? Tout est possible ici !
+            </h2>
+            <div className="m-auto w-3/4 grid auto-rows-fr gap-4 mt-8 mb-8">
+              <Button
+                className="h-fit bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+                onPress={() => delay(navigate, 300, "/boardgames/search")}
+              >
+                <div className="grid grid-rows-2 gap-4 items-center p-2">
+                  <div className="m-auto">
+                    <DbIcon />
+                  </div>
+                  <div>
+                    <div className="font-bold text-2xl pb-4">
+                      <FormattedMessage id="Home.Orop" />
+                    </div>
+                    <div className="italic text-wrap">
+                      <FormattedMessage id="Home.Orop.Subtitle" />
+                    </div>
+                  </div>
+                </div>
+              </Button>
+
+              <Button
+                className="h-fit bg-gradient-to-tr from-fuchsia-500 to-cyan-500 text-white shadow-lg"
+                onPress={() => delay(navigate, 300, "/my-account/ratings")}
+              >
+                <div className="grid grid-rows-2 gap-4 items-center p-2">
+                  <div className="m-auto">
+                    <MyRatingsIcon />
+                  </div>
+                  <div>
+                    <div className="font-bold text-2xl pb-4">
+                      <FormattedMessage id="Home.MyRatings" />
+                    </div>
+                    <div className="italic text-wrap">
+                      <FormattedMessage id="Home.MyRatings.Subtitle" />
+                    </div>
+                  </div>
+                </div>
+              </Button>
+
+              <Button
+                className="h-fit bg-gradient-to-tr from-cyan-500 to-yellow-500 text-white shadow-lg"
+                onPress={() => delay(navigate, 300, "/boardgames/top/asked")}
+              >
+                <div className="grid grid-rows-2 gap-4 items-center p-2">
+                  <div className="m-auto">
+                    <AskOropIcon />
+                  </div>
+                  <div>
+                    <div className="font-bold text-2xl pb-4">
+                      <FormattedMessage id="Home.VoteForOrop" />
+                    </div>
+                    <div className="italic text-wrap">
+                      <FormattedMessage id="Home.VoteForOrop.Subtitle" />
+                    </div>
+                  </div>
+                </div>
+              </Button>
             </div>
           </div>
         </CardBody>
